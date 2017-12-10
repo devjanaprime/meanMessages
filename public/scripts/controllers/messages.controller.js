781047e483069d63833cb1f1f2dfef654fa3fdb8
devjanaprime <dev@primeacademy.io>
2017-12-10 14:06:01 -0600

    started solve

--- public/scripts/controllers/messages.controller.js
+++ public/scripts/controllers/messages.controller.js
@@ -1,4 +1,28 @@
-myApp.controller( 'MessagesController', [ 'MessagesService', ( MessagesService ) => {
+myApp.controller( 'MessagesController', [ 'MessagesService', function( MessagesService ) {
     console.log( 'NG' );
     let vm=this;
+    vm.userName = '';
+
+    vm.logIn = () => {
+        if( vm.userName == '' && vm.nameIn != '' ){
+            vm.userName = vm.nameIn;
+        }
+    } // end log in
+
+    vm.logOut = () => {
+        vm.userName = '';
+        vm.nameIn = '';
+    } // end log in
+
+    vm.getMessages = () => {
+        console.log( 'in service getMessage' );
+        MessagesService.getMessages().then( () =>{
+            vm.messages = MessagesService.messages;
+            console.log( vm.messages );
+        }); // end service call
+    } // end getMessages
+
+    vm.postMessage = () => {
+    
+    } // end getMessages
 }]);
--- public/scripts/services/messages.service.js
+++ public/scripts/services/messages.service.js
@@ -1,3 +1,14 @@
 myApp.service( 'MessagesService', function( $http ){
     let sv=this;
+
+    sv.getMessages = () => {
+        console.log( 'in service getMessage' );
+        return $http({
+            method: 'GET',
+            url: '/messages'
+        }).then( ( response ) => {
+            console.log( 'back from server with:', response );
+            sv.messages = response;
+        });
+    }
 });
